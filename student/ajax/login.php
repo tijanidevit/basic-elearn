a295790b79f12c9ba74eaf45e1c4c20b29bf6173
TijaniDevIt <tijanidevit@yahoo.com>
2021-07-14 03:45:07 +0100

    StudentLogin

--- student/ajax/login.php
+++ student/ajax/login.php
@@ -1,31 +1,28 @@
 <?php 
-include_once '../../core/session.class.php';
-include_once '../../core/admin.class.php';
-include_once '../../core/core.function.php';
+	include_once '../../core/session.class.php';
+	include_once '../../core/students.class.php';
+	include_once '../../core/core.function.php';
+	echo register();
+	function register()
+	{
+		$session = new Session();
+		$student_obj = new Students();
+		if (isset($_POST['email'])) {
+			$email = $_POST['email'];
+			$password = md5($_POST['password']);
 
-$session = new Session();
-$admin_obj = new Admin();
 
-if (isset($_POST['email'])) {
-	$email = $_POST['email'];
-	$password = md5($_POST['password']);
-
-	if ($admin_obj->check_email($email)) {
-		if ($admin_obj->admin_login($email,$password)) {
-			$admin = $admin_obj->fetch_admin($email);
-			$session->create_session('bike_admin',$admin);
-			echo 1;
-		}
-		else{
-			echo displayWarning('Invalid Password');
+			if (! $student_obj->check_email($email)) {
+				return displayWarning('Email address not found!');
+			}
+			if ($student_obj->student_login($email,$password)) {
+				$student = $student_obj->fetch_student($email);
+				$session->create_session('elearn_student',$student);
+				return 1;
+			}
+			else{
+				return displayWarning('Invalid Login Credentials.');
+			}
 		}
 	}
-	else{
-		echo displayWarning('Email address not recognised');
-	}
-}
-
-else{
-	echo "No input made";
-}
 ?>

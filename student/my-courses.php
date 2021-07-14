1bbba0126c941b2979a4f81758adb2574208fd5b
TijaniDevIt <tijanidevit@yahoo.com>
2021-07-14 18:20:48 +0100

    Course

--- student/my-courses.php
+++ student/my-courses.php
@@ -55,22 +55,18 @@ $student_courses = $student_obj->fetch_student_courses($student_id);
 
                                         <?php foreach ($student_courses as $course): ?>
                                             <div class="col-lg-6 col-xl-3 mb-3">
-                                                <!-- Simple card -->
-                                                <div class="col-lg-6 col-xl-3 mb-3">
-                                                    <!-- Simple card -->
-                                                    <div class="card bg-light mb-4 mb-xl-0">
-                                                        <img class="card-img-top img-fluid" src="../uploads/course/image/<?php echo $course['course_image'] ?>" alt="Card image cap">
-                                                        <div class="card-body">
-                                                            <h5 class="card-title font-size-16"><?php echo $course['course_title'] ?></h5>
-                                                            <p class="card-text text-muted"><?php echo substr($course['course_description'],0,120) ?>...</p>
-                                                            <div class="media-body">
-                                                                <div class="text-muted font-weight-normal mt-1 mb-4">
-                                                                    <div><i class='uil uil-user'></i> <?php echo $course['fullname'] ?></div>
-                                                                    <i class='uil uil-calendar-alt'></i> <?php echo format_date($course['created_at']) ?>
-                                                                </div>
+                                                <div class="card bg-light mb-4 mb-xl-0">
+                                                    <img class="card-img-top img-fluid" src="../uploads/course/image/<?php echo $course['course_image'] ?>" alt="Card image cap">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title font-size-16"><?php echo $course['course_title'] ?></h5>
+                                                        <p class="card-text text-muted"><?php echo substr($course['course_description'],0,120) ?>...</p>
+                                                        <div class="media-body">
+                                                            <div class="text-muted font-weight-normal mt-1 mb-4">
+                                                                <div><i class='uil uil-user'></i> <?php echo $course['fullname'] ?></div>
+                                                                <i class='uil uil-calendar-alt'></i> <?php echo format_date($course['created_at']) ?>
                                                             </div>
-                                                            <a href="course-class?id=<?php echo $course['id'] ?>" class="text-primary mr-3">Go to course</a>
                                                         </div>
+                                                        <a href="course-class?id=<?php echo $course['id'] ?>" class="text-primary mr-3">Go to course</a>
                                                     </div>
                                                 </div>
                                             </div>
